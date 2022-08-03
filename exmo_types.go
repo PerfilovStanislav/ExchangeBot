74ca5a8e7c8e525eebc1a46c5e0004a5583e5ccb
stas <stanislav.perfilov@gmail.com>
2022-08-03 16:05:51 +0300

    fix

--- exmo_types.go
+++ exmo_types.go
@@ -25,10 +25,11 @@ func (order OpenedOrder) isEmpty() bool {
 type Currency string
 
 const (
-	ALGO Currency = "ALGO"
-	EXM  Currency = "EXM"
-	USD  Currency = "USD"
 	USDT Currency = "USDT"
-	RUB  Currency = "RUB"
 	ETC  Currency = "ETC"
 )
+
+type CurrencyBalanceResponse struct {
+	USDT Price `json:"USDT,string"`
+	ETC  Price `json:"ETC,string"`
+}
